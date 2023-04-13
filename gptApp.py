131662e07ed6c8bb25015ee702a9c80b483c7ba0
GageHowe <howe.gaged@gmail.com>
2023-04-13 16:17:57 -0500

    changed model to davinci 3

--- gptApp.py
+++ gptApp.py
@@ -1,16 +1,24 @@
-print("hello")
-
 import os
 import openai
-openai.api_key = os.getenv("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")
 
-user_input = input("Enter your prompt for ChatGPT: ")
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": user_input}
-    ]
+with open("openai_api_key.txt", "r") as f:
+    openai_api_key = f.read()
+    print(openai_api_key)
+
+
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv(openai_api_key)
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="hello!",
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
 )
 
-print(response.choices[0].message.content)
+text = response['choices'][0]['text']
+print(text)
